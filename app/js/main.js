bf7d0624465e10183c34751b9e792cc6ca6fd7a1
xyrkep <serge@coudeau.fr>
2020-02-13 08:55:57 +0100

    added product slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,21 @@
-$(function(){
+$(function () {
 
-var mixer = mixitup('.products__inner-box');
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+
+    });
+
+
+
+    var mixer = mixitup('.products__inner-box');
 
 });
