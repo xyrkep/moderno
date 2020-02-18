d852dbee9cffc92205497a1b219b3852f63e978a
xyrkep <serge@coudeau.fr>
2020-02-18 09:35:19 +0100

    mobile adaptation header&footer is done

--- app/js/main.js
+++ app/js/main.js
@@ -36,6 +36,14 @@ $(function () {
         $('.icon-th-large').addClass('active');
     });
 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function () {
+        $('.header__box').toggleClass('active');
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
