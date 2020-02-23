85451701766bdf2f2fdb9bac38aacf61b2edb47c
xyrkep <serge@coudeau.fr>
2020-02-23 14:14:36 +0100

    added upload page

--- app/js/main.js
+++ app/js/main.js
@@ -53,7 +53,7 @@ $(function () {
         return false;
     });
 
-
+    $('input[type="file"], select').styler();
 
 
     var mixer = mixitup('.products__inner-box');
--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ gulp.task('style', function () {
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
         'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+        'node_modules/jquery.formstyler/jquery.formstyler.css'
 
     ])
         .pipe(concat('libs.min.css'))
@@ -46,7 +47,8 @@ gulp.task('script', function () {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/jquery.formstyler/jquery.formstyler.js'
 
     ])
         .pipe(concat('libs.min.js'))
