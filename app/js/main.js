3e4f4c6db8e44d0d09faa440d49c13fba4c9282b
xyrkep <serge@coudeau.fr>
2020-02-25 23:02:08 +0100

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,36 @@ $(function () {
         dots: true,
         arrows: false,
         slidesToShow: 4,
-        slidesToScroll: 4
-
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
